9cc97e7e151705fbda784e8bf36238179fadfd18
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2025-05-13 21:12:48 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const driver = require('./db');
+const path = require('path');
+
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 
 const getSession = () => driver.session();
 
